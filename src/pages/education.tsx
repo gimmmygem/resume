40deffda3383fe75e9c2007da54df700fb016ae1
gimmmygem <gemhannan.creates@gmail.com>
2024-04-07 13:36:32 +1000

    Update to education

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -15,13 +15,13 @@ export default function Home() {
         <title>Resume | Gem Hannan</title>
         <meta
           name="description"
-          content="Resume - Content Specialist & Web Designer | Gem Hannan."
+          content="Resume - Content & Education Specialist | Gem Hannan."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Info subtitle="Heckin doodle" tagline="King of doodles" />
+        <Info subtitle="Content & Education Specialist" tagline="Intuitive problem solver and creative thinker — demonstrated success in delivering ground-breaking projects and empowering others to thrive in their work." />
         <Skills sections={[
           {
             heading: "Content & Marketing",
@@ -67,36 +67,57 @@ export default function Home() {
         ]} />
         <Education />
         <WorkHistory />
-        <WorkHistory title="Volunteering" roles={[{
-          title: "Web Designer (Contract)",
-          employer: "Avivo Australia",
-          start: "Dec 2023",
-          end: "Feb 2024",
+        <WorkHistory title="Volunteer Work" roles={[{
+          title: "Grade 6 Camp at The Ranch, Mornington Peninsula",
+          employer: "Balwyn North Primary School",
+          start: "",
+          end: "2024",
           description:
-            "Contracted to deliver a new website for Avivo Elite Tutoring using Wordpress CMS.",
+            "Volunteer supervisor on 3 day camp",
           responsibilities: [
-            "Defining and delivering website, SEO & SEM strategy to engage current and potential clients",
-            "Concepting original website design and creating flexible webpage templates",
-            "Establishing and maintaining responsive design standards",
-            "Copywriting based on established brand guidelines",
-            "Optimisation and QA testing across desktop, tablet and mobile",
-            "Upskilling Avivo team in ongoing website content management",
+            "Supporting BNPS staff members with travel, activities & behaviour management",
+            "Managing assigned groups when staff members needed elsewhere",
+            "Upholding BNPS child safe and other policies",
           ],
         },
         {
-          title: "Web Designer (Contract)",
-          employer: "Avivo Australia",
-          start: "Dec 2023",
-          end: "Feb 2024",
+          title: "Mirabel Family Camp",
+          employer: "Lord Somers Camp and Power House | Mirabel Foundation",
+          start: "2017",
+          end: "2018",
           description:
-            "Contracted to deliver a new website for Avivo Elite Tutoring using Wordpress CMS.",
+            "Volunteer staff member on a weekend camp for children and families impacted by drug or substance abuse.",
           responsibilities: [
-            "Defining and delivering website, SEO & SEM strategy to engage current and potential clients",
-            "Concepting original website design and creating flexible webpage templates",
-            "Establishing and maintaining responsive design standards",
-            "Copywriting based on established brand guidelines",
-            "Optimisation and QA testing across desktop, tablet and mobile",
-            "Upskilling Avivo team in ongoing website content management",
+            "Managing and supervising actvities for the children",
+            "Supporting other volunteer staff members in their assigned roles",
+            "Upholding the expectations, policies and procedures of LSCPH and Mirabel Foundation",
+          ],
+        },
+        {
+          title: "Very Special Kids (VSK) Camp",
+          employer: "Lord Somers Camp and Power House | Very Special Kids Foundation",
+          start: "2018",
+          end: "2019",
+          description:
+            "Volunteer staff member of a weekend camp for children or young people affected by life-limiting conditions, and their families.",
+          responsibilities: [
+            "Managing and supervising actvities for the children",
+            "Supporting other volunteer staff members in their assigned roles",
+            "Upholding the expectations, policies and procedures of LSCPH and the Very Special Kids Foundation",
+          ],
+        }
+        {
+          title: "Lady Somers Camp | Big Camp",
+          employer: "Lord Somers Camp and Power House",
+          start: "2015",
+          end: "2017",
+          description:
+            "Volunteer staff member on a week long camp for 100 young women, female identifying or non-binary youth. The aim of Big Camp is to challenge the perspectives that young people hold about themselves and others.",
+          responsibilities: [
+            "Offering support and encouragement to all groupers throughout a diverse range of activities and challenges",
+            "Performing all assigned tasks within team and reporting to team leader",
+            "Supporting other volunteer staff members in their roles",
+            "Upholding the expectations, policies and procedures of LSCPH",
           ],
         }]} />
         <Projects />
